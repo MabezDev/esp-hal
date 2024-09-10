929f5d51d93893da0caefd92d8fd53e44a86149d
Kirill Mikhailov <konnor1980@yandex.ru>
2024-09-10 17:44:33 +0200

    WIP

--- esp-wifi/src/wifi/mod.rs
+++ esp-wifi/src/wifi/mod.rs
@@ -66,6 +66,9 @@ use crate::{
         peripheral::{Peripheral, PeripheralRef},
     },
     EspWifiInitialization,
+    EspWifiOperationFor,
+    EspWifiDeinitialization,
+    EspWifiDeinitializationInternal,
 };
 
 const ETHERNET_FRAME_HEADER_SIZE: usize = 18;
@@ -92,6 +95,7 @@ use crate::{
             esp_wifi_disconnect,
             esp_wifi_get_mode,
             esp_wifi_init_internal,
+            esp_wifi_deinit_internal,
             esp_wifi_internal_free_rx_buffer,
             esp_wifi_internal_reg_rxcb,
             esp_wifi_internal_tx,
@@ -1401,6 +1405,29 @@ pub(crate) fn wifi_start() -> Result<(), WifiError> {
     }
 
     Ok(())
+}
+
+/// Stops and de-initializes Wi-Fi stack
+pub fn deinitialize(
+    deinit_for: EspWifiOperationFor,
+    controller: WifiController, 
+    stack: crate::wifi_interface::WifiStack<impl WifiDeviceMode>,
+) -> Result<EspWifiDeinitialization, WifiError> {
+
+    esp_wifi_result!(unsafe { esp_wifi_stop() })?;
+    esp_wifi_result!(unsafe { esp_wifi_deinit_internal() })?;
+
+    drop(controller);
+    drop(stack);
+
+    match deinit_for {
+        #[cfg(feature = "wifi")]
+        EspWifiOperationFor::Wifi => Ok(EspWifiDeinitialization::Wifi(EspWifiDeinitializationInternal)),
+        #[cfg(feature = "ble")]
+        EspWifiOperationFor::Ble => Ok(EspWifiDeinitialization::Ble(EspWifiDeinitializationInternal)),
+        #[cfg(coex)]
+        EspWifiOperationFor::WifiBle => Ok(EspWifiDeinitialization::WifiBle(EspWifiDeinitializationInternal)),
+    }
 }
 
 unsafe extern "C" fn coex_register_start_cb(
